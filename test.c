a415643315ccd465e05f931147be8f3a31a0f604
Aleksey Vlasov <aleksey.vlasov@gmail.com>
2015-10-13 20:50:34 +0000

    Expanded the case statement

--- test.c
+++ test.c
@@ -29,6 +29,12 @@ main()
             ObjectData[currentObject] = 0xBB;
          case DATA_FLAGS:
             ObjectData[currentObject] = 0xCC;
+         case DATA_NUM_ELEMENTS:
+            ObjectData[currentObject] = 0xDD;
+            break;
+         default:
+           printf("\n Shouldn't get here!!! CurrentObject=%x \n", currentObject);
+           break;
       }
       currentObject++;
    }
